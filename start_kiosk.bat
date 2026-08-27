a7196882050377181398f4945adccc73b8afbba3
fadli <fahrezifadli57@gmail.com>
2026-08-27 08:19:40 +0700

    menambahkan shortcut dan ikon

--- start_kiosk.bat
+++ start_kiosk.bat
@@ -1,4 +1,21 @@
 @echo off
+
+:: Bikin Shortcut Desktop Otomatis Jika Belum Ada
+set "SHORTCUT_PATH=%USERPROFILE%\Desktop\R.I.M.A.lnk"
+if not exist "%SHORTCUT_PATH%" (
+    echo Mendaftarkan R.I.M.A ke Desktop...
+    echo Set oWS = WScript.CreateObject^("WScript.Shell"^) > "%temp%\CreateShortcut.vbs"
+    echo sLinkFile = "%SHORTCUT_PATH%" >> "%temp%\CreateShortcut.vbs"
+    echo Set oLink = oWS.CreateShortcut^(sLinkFile^) >> "%temp%\CreateShortcut.vbs"
+    echo oLink.TargetPath = "%~dp0start_kiosk.bat" >> "%temp%\CreateShortcut.vbs"
+    echo oLink.WorkingDirectory = "%~dp0" >> "%temp%\CreateShortcut.vbs"
+    echo oLink.IconLocation = "%~dp0rima_icon.ico, 0" >> "%temp%\CreateShortcut.vbs"
+    echo oLink.Save >> "%temp%\CreateShortcut.vbs"
+    
+    cscript /nologo "%temp%\CreateShortcut.vbs"
+    del "%temp%\CreateShortcut.vbs"
+)
+
 echo ==============================================
 echo       SORA ANGKLUNG - KIOSK STARTUP
 echo ==============================================
@@ -13,7 +30,8 @@ timeout /t 5 /nobreak >nul
 :: 2. Membuka Google Chrome dalam Kiosk Mode (Layar penuh, tanpa navbar)
 echo [2/2] Membuka antarmuka PWA dalam Mode Kiosk...
 :: Asumsi Chrome terinstal di direktori default
-start chrome --user-data-dir="%temp%\angklung_chrome_profile" --start-fullscreen --app="http://localhost:8000" --disable-pinch --overscroll-history-navigation=0
+start chrome --user-data-dir="%temp%\angklung_chrome_profile" --kiosk "http://localhost:8000" --disable-pinch --overscroll-history-navigation=0
 
 echo Selesai!
 exit
+    
